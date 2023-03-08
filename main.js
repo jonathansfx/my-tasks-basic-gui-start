2f6cf51406dc90ac1ba11fb35d523fdb7206c13b
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-08 11:20:52 -0700

    asdf

--- main.js
+++ main.js
@@ -1,41 +1,107 @@
 // My Tasks Basic
 
 // HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let tasksEl = document.getElementById('tasks');
+let goBtnEl = document.getElementById("go-btn");
+let menuEl = document.getElementById("menu");
+let tasksEl = document.getElementById("tasks");
+
+// Global Variables
+let tasks = loadTasks();
+displayAll();
 
 // Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
+goBtnEl.addEventListener("click", goBtnHandler);
 
 function goBtnHandler() {
   // Get Menu Selection
   let selection = menuEl.value;
 
-  if (selection === 'add') {
+  if (selection === "add") {
     addTask();
-  } else if (selection === 'toggle') {
+  } else if (selection === "toggle") {
     toggleTask();
-  } else if (selection === 'remove') {
+  } else if (selection === "remove") {
     removeTask();
-  } else if (selection === 'clear') {
+  } else if (selection === "clear") {
     clearAll();
   }
 }
 
 // MENU FUNCTIONS
 function addTask() {
-  console.log('Add Task');
+  let description = prompt("enter task description:");
+  tasks.push(newTask(description));
+  saveTasks();
+  displayAll();
 }
 
+// toggle completed status of a task
 function toggleTask() {
-  console.log('Toggle Task');
+  let index = +prompt("Enter # of task:");
+  let task = tasks[index];
+  if (task.completed === "") {
+    task.completed = "completed";
+  } else {
+    task.completed = "";
+  }
+  saveTasks();
+  displayAll();
 }
 
+// Remove a task by index
 function removeTask() {
-  console.log('Remove Task');
+  let index = +prompt("Enter # of task:");
+  if (index >= 0 && index < tasks.length) {
+    // Valid index -> Remove
+    tasks.splice(index, 1);
+    saveTasks();
+    displayAll();
+  } else {
+    alert("Invalid Task #");
+  }
 }
 
+// Remove all tasks
 function clearAll() {
-  console.log('Clear All');
+  tasks = [];
+  saveTasks();
+  displayAll();
+}
+
+// HELPER FUNCTIONS
+
+// Return a new task object
+function newTask(taskDescription) {
+  return {
+    description: taskDescription,
+    completed: "",
+  };
+}
+
+// Display all tasks in global tasks array
+function displayAll() {
+  let outputStr = "";
+  for (let i = 0; i < tasks.length; i++) {
+    outputStr += getTaskHTMLStr(tasks[i], i);
+  }
+  tasksEl.innerHTML = outputStr;
+}
+
+// Get HTML for given task
+function getTaskHTMLStr(task, i) {
+  return `
+  <div class = "${task.completed}">
+    ${i}: ${task.description}
+  </div>
+  `;
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// load tasks from local storage
+function loadTasks() {
+  let tasksStr = localStorage.getItem("tasks");
+  return JSON.parse(tasksStr) ?? [];
 }
